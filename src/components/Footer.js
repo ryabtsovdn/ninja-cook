3e60cb8a5122d7aeeb043f1eb3cd18ab425ac4a8
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2023-02-24 21:04:55 +0300

    Localize homepage/navbar/footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useTranslation, Trans } from "gatsby-plugin-react-i18next";
 
 function Footer() {
+  const { t } = useTranslation();
+
   return (
     <footer className="page-footer">
       <p>
-        &copy; {new Date().getFullYear()} <span>Ninja Recipes</span>. Built with{" "}
-        <a href="https://gatsbyjs.com/">Gatsby</a>
+        &copy; {new Date().getFullYear()} <span>{t("title")}</span>.{" "}
+        <Trans
+          i18nKey="buildWithGatsby"
+          components={{ L: <a href="https://gatsbyjs.com/">Gatsby</a> }}
+        />
       </p>
     </footer>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,48 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { useTranslation } from "gatsby-plugin-react-i18next";
 import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
 import AllRecipes from "../components/AllRecipes";
 import Meta from "../components/Meta";
 
-export const Head = () => <Meta />;
+export const Head = ({ pageContext: { language } }) => (
+  <Meta language={language} />
+);
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
 
 function Home() {
+  const { t } = useTranslation();
+
   return (
     <Layout>
       <main className="page">
         <header className="hero">
           <StaticImage
             src="../assets/images/main.jpeg"
-            alt="Grill"
+            alt={t("grill")}
             className="hero-img"
             placeholder="dominantColor"
             layout="fullWidth"
           />
           <div className="hero-container">
             <div className="hero-text">
-              <h1>Ninja Cook</h1>
-              <h4>No fluff, just recipes</h4>
+              <h1>{t("title")}</h1>
+              <h4>{t("slogan")}</h4>
             </div>
           </div>
         </header>
