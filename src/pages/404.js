6f1b1c45b30269d80f29b7ce29a295ee1614a1b5
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2023-02-25 11:18:14 +0300

    Localize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,37 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { useTranslation } from "gatsby-plugin-react-i18next";
 
 import Layout from "../components/Layout";
 import Meta from "../components/Meta";
 
-export const Head = () => <Meta title="Ninja Cook - Page Not Found" />;
+export const Head = ({ pageContext: { language } }) => (
+  <Meta title="Ninja Cook - Page Not Found" language={language} />
+);
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
 
 function NotFound() {
+  const { t } = useTranslation();
+
   return (
     <Layout>
       <main className="error-page">
         <section>
           <h1>404</h1>
-          <h3>Page not found</h3>
+          <h3>{t("app:pageNotFound")}</h3>
         </section>
       </main>
     </Layout>
