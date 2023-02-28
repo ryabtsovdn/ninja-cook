dbd73d3b2885afec5ac2346a1eaee23fa121de66
Dmitriy Ryabtsov <ryabtsovdn@yandex.ru>
2023-02-28 16:39:48 +0300

    Add language switcher to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { useTranslation, Trans } from "gatsby-plugin-react-i18next";
 
+import LanguageSwitcher from "./LanguageSwitcher";
+
 function Footer() {
   const { t } = useTranslation();
 
   return (
     <footer className="page-footer">
-      <p>
-        &copy; {new Date().getFullYear()} <span>{t("app:title")}</span>.{" "}
-        <Trans
-          i18nKey="app:buildWithGatsby"
-          components={{ L: <a href="https://gatsbyjs.com/">Gatsby</a> }}
-        />
-      </p>
+      <div className="container-center">
+        <p>
+          &copy; {new Date().getFullYear()}{" "}
+          <span className="highlight">{t("app:title")}</span>.{" "}
+          <span className="made">
+            <Trans
+              i18nKey="app:buildWithGatsby"
+              components={{
+                L: (
+                  <a href="https://gatsbyjs.com/" className="highlight">
+                    Gatsby
+                  </a>
+                ),
+              }}
+            />
+          </span>
+        </p>
+        <LanguageSwitcher />
+      </div>
     </footer>
   );
 }
